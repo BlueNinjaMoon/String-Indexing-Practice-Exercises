05c2a61d65ddcf5c7df9743671d992c57abbba62
ChristianLehman <cmp_chlehman@ects.org>
2026-09-11 09:34:03 -0400

    Exercise5Done

--- phrase_slices.py
+++ phrase_slices.py
@@ -1,3 +1,5 @@
-phrase = 
+phrase = input("Enter a phrase: ").strip()
 
-print()
+print(f"First three characters: {phrase[0:3]}")
+print(f"The last three characters: {phrase[-3:]}")
+print(f"Every second charactert: {phrase[0::2]}")
